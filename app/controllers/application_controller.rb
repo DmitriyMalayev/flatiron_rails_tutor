fb8b55e7d25ef5c2aa7420a9eca0154062f3957a
DmitriyMalayev <dmitriy.malayev@gmail.com>
2020-11-08 16:15:03 -0500

    Utilized Path Helpers, Added Navigation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,3 +6,18 @@ class ApplicationController < ActionController::Base
     def configure_permitted_parameters
         devise_parameter.sanitizer.permit(:sign_up, keys: [:username])  
     end  
+end 
+
+
+
+# class CreateStudents < ActiveRecord::Migration[6.0]
+#   def change
+#     create_table :students do |t|
+#       t.string :name
+#       t.string :phone_number
+#       t.string :email 
+
+#       t.timestamps
+#     end
+#   end
+# end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_08_193601) do
+ActiveRecord::Schema.define(version: 2020_11_08_210344) do
+
+  create_table "students", force: :cascade do |t|
+    t.string "name"
+    t.string "phone_number"
+    t.string "email"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_students_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,4 +36,5 @@ ActiveRecord::Schema.define(version: 2020_11_08_193601) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "students", "users"
 end
